0e7707bbb757f3407489ec8a6a21bd74c0657bae
Daniil Palchikov <Daniels.developer48@gmail.com>
2023-06-15 21:36:31 +0300

    last result

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,66 +1,68 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-from re import findall, search
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from bs4 import BeautifulSoup
+# from re import findall, search
+import requests
+import datetime
 import os
 
 
-def get_params(bs4_obj):
-    bs4_content = list(list(bs4_obj.children)[2].children)
-    obj = {}
-    body = list(bs4_content[1].children)
-    slider = bs4_content[0]
-    obj["image"] = search(r"https://[^\s\"\']+", string=str(slider)).group(0)
-    name_item = "iva-item-"
-    for item in body[::-1]:
-        string = str(item)
-        if findall(r"data-marker=[\"\']item-line[\"\']", string):
-            obj["agent"] = item.text
-        if findall(f"{name_item}description", string):
-            obj["description"] = item.text
-        if findall(f"{name_item}developmentNameStep", string):
-            obj["address"] = item.text
-        if findall(f"{name_item}autoParamsStep", string):
-            obj["conditions"] = item.text
-        if findall(f"{name_item}dateInfoStep", string):
-            obj["time"] = item.text
-        if findall(f"{name_item}titleStep", string):
-            obj["rooms"] = item.text
-        if findall(f"{name_item}priceStep", string):
-            obj["price"] = item.text
-    return obj
+key = "ee274a7924d788abb8dac7c9e00eb690"
 
 
-driver = webdriver.Chrome(executable_path="./chrome/chromedriver.exe")
-url = "https://www.avito.ru/anapa/kvartiry/sdam/na_dlitelnyy_srok"
-try:
-    driver.get(url=url)
-    list_elements = driver.find_elements(By.CSS_SELECTOR, value='[data-marker="item"]')
-    with open("./page.html", mode="w+", encoding="utf-8") as file:
-        for elem in list_elements:
-            file.write(f"<div class='item'> {elem.get_property('innerHTML')} </div>")
+def get_geocoding(city: str) -> dict:
+    url_geocoding = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={key}"
+    response_geocoding = requests.get(url=url_geocoding).json()[0]
+    return {"lat": response_geocoding.get("lat"), "lon": response_geocoding.get("lon")}
 
-        soup_obj = BeautifulSoup(file.read(), "html.parser")
-        list_children = list(soup_obj.children)
-        obj = []
 
-        for items in list_children:
-            params = get_params(items)
-            print(params)
+def get_wheather(city: str) -> str:
+    geocode = get_geocoding(city)
+    lat = geocode.get("lat")
+    lon = geocode.get("lon")
+    url_weather = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={key}&lang=ru&units=metric"
+    response = requests.get(url=url_weather).json()
+    return response
 
-except Exception as c:
-    print(c)
-finally:
-    driver.close()
-    driver.quit()
+
+def get_response_for_telegram(obj: dict) -> None:
+    list_weather = obj.get("list")
+    city = obj.get("city").get("name")
+    coord = obj.get("city").get("coord")
+
+    sunrise = datetime.datetime.fromtimestamp(obj.get("city").get("sunrise"))
+    sunset = datetime.datetime.fromtimestamp(obj.get("city").get("sunset"))
+    during_day = sunset - sunrise
+    for item in list_weather:
+        time = datetime.datetime.fromtimestamp(item.get("dt"))
+        main = item.get("main")
+        temp = main.get("temp")
+        pressure = main.get("pressure")
+        sea_level = main.get("sea_level")
+        humidity = main.get("humidity")
+        description = item.get("weather")[0].get("description")
+        icon = item.get("weather")[0].get("icon")
+        wind_speed = item.get("wind").get("speed")
+        wind_gust = item.get("wind").get("gust")
+        repsonse = f"*** {datetime.datetime.now().strftime('%d-%m-%Y %H:%M')} ***\n" \
+                         f"Погода в {city}e на {time.strftime('%d-%m-%Y %H:%M')}\n" \
+                         f"Описание: {description}" \
+                         f"\nТемпература: {temp} градусов\n" \
+                         f"Давление: {pressure} мм.рт.ст.\nУровень моря: {sea_level} м\n" \
+                         f"Влажность: {humidity}%\nСкорость ветра: {wind_speed} м/с\nПорывы ветра: {wind_gust} м/с\n" \
+                         f"Восход солнца: {sunrise.strftime('%H:%M')}\nЗаход солнца: {sunset.strftime('%H:%M')}\nПродолжительность дня: {during_day}\n" \
+                   f"Широта: {coord.get('lat')}\nДолгота: {coord.get('lon')}"
+        return repsonse
 
 
 bot = Bot(token=os.getenv("TOKEN"))
 
 dp = Dispatcher(bot)
+
 
 @dp.message_handler()
 async def function(message: types.Message):
@@ -69,8 +71,8 @@ async def function(message: types.Message):
 
 
 def get_data_from_db(message):
-    list_city = {"анапа": "anapa", "иваново": "ivanovo"}
-
+    raw_data = get_wheather(message)
+    return get_response_for_telegram(raw_data)
 
 
 executor.start_polling(dp, skip_updates=True)
